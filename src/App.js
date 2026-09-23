bcbcb84160bb47e90d957d0cb215ce13ca13b032
Eona123 <adrienne.wellinger@students.fhnw.ch>
2026-09-23 11:58:20 +0200

    second test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ export default function App() {
   return (
     <div>
       <div>Hallo Mars</div>
+
+      <button> id ="Mein Button" text </button>
+      <button> id ="Mein Button" disabled text </button>
+      <input/> 
     </div>
   );
 }
